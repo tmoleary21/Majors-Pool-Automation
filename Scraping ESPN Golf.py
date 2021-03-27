7439dd252d90e287af9db01e04d1219cfb863cca
Tyson O'Leary <olearytyson@gmail.com>
2021-03-26 22:22:10 -0600

    Made quitting easier to understand

--- Scraping ESPN Golf.py
+++ Scraping ESPN Golf.py
@@ -69,64 +69,71 @@ def decideTie(tally, winnerIndices, tournamentWinnerScore):
             winner = winnerIndices[i]
     return winner
 
-
-responses = readResponses()
-# print(str(responses) + '\n')
-
-
-leaderboardHTML = accessLeaderboard()
-# leaderboardHTML = accessLeaderboard('/_/tournamentId/401219793/season/2020') #To test 2019-20
+def run():
+    responses = readResponses()
+    # print(str(responses) + '\n')
 
 
-# Choose Tournament
-tournaments = getTournaments(leaderboardHTML)
-for i in range(min(15, len(tournaments))):
-    print(str(i + 1) + ' - ' + tournaments[i])
-tournament = tournaments[int(input()) - 1]
-# print(tournament)
-tournamentHtml = accessSpecificLeaderboard(leaderboardHTML, tournament)
+    leaderboardHTML = accessLeaderboard()
+    # leaderboardHTML = accessLeaderboard('/_/tournamentId/401219793/season/2020') #To test 2019-20
 
-winnerIndices = [0]
-tally = []
-for i in range(1, len(responses)):
-    sum = 0
-    temp = []
-    temp.append(responses[i][0])  # ......................................................................First Name
-    temp.append(responses[i][1])  # ......................................................................Last Name
-    temp.append(responses[i][2])  # ......................................................................Golfer 1 Name
-    temp.append(getEarnings(responses[i][2], tournamentHtml))  # .........................................Golfer 1 Score
-    sum += temp[3] if temp[3] != "NIT" else 0
-    temp.append(responses[i][3])  # ......................................................................Golfer 2 Name
-    temp.append(getEarnings(responses[i][3], tournamentHtml))  # .........................................Golfer 2 Score
-    sum += temp[5] if temp[5] != "NIT" else 0
-    temp.append(responses[i][4])  # ......................................................................Golfer 3 Name
-    temp.append(getEarnings(responses[i][4], tournamentHtml))  # .........................................Golfer 3 Score
-    sum += temp[7] if temp[7] != "NIT" else 0
-    temp.append(sum)  # ..................................................................................Sum of earnings
-    temp.append(int(responses[i][5]))  # ......................................................................Score guess
-    tally.append(temp)
-    if i > 1:
-        if sum == tally[winnerIndices[0]][8]:
-            winnerIndices.append(i - 1)
-        if sum > tally[winnerIndices[0]][8]:
-            winnerIndices = [i - 1]
 
-# for t in tally:
-#     print(t)
+    # Choose Tournament
+    tournaments = getTournaments(leaderboardHTML)
+    for i in range(min(15, len(tournaments))):
+        print(str(i + 1) + ' - ' + tournaments[i])
+    tournament = tournaments[int(input()) - 1]
+    # print(tournament)
+    tournamentHtml = accessSpecificLeaderboard(leaderboardHTML, tournament)
 
-#print(winnerIndices)
-winnerIndex = decideTie(tally, winnerIndices, getWinnerScore(tournamentHtml))
-print('\nWINNER: ' + str(tally[winnerIndex]))
+    winnerIndices = [0]
+    tally = []
+    for i in range(1, len(responses)):
+        sum = 0
+        temp = []
+        temp.append(responses[i][0])  # ......................................................................First Name
+        temp.append(responses[i][1])  # ......................................................................Last Name
+        temp.append(responses[i][2])  # ......................................................................Golfer 1 Name
+        temp.append(getEarnings(responses[i][2], tournamentHtml))  # .........................................Golfer 1 Score
+        sum += temp[3] if temp[3] != "NIT" else 0
+        temp.append(responses[i][3])  # ......................................................................Golfer 2 Name
+        temp.append(getEarnings(responses[i][3], tournamentHtml))  # .........................................Golfer 2 Score
+        sum += temp[5] if temp[5] != "NIT" else 0
+        temp.append(responses[i][4])  # ......................................................................Golfer 3 Name
+        temp.append(getEarnings(responses[i][4], tournamentHtml))  # .........................................Golfer 3 Score
+        sum += temp[7] if temp[7] != "NIT" else 0
+        temp.append(sum)  # ..................................................................................Sum of earnings
+        temp.append(int(responses[i][5]))  # ......................................................................Score guess
+        tally.append(temp)
+        if i > 1:
+            if sum == tally[winnerIndices[0]][8]:
+                winnerIndices.append(i - 1)
+            if sum > tally[winnerIndices[0]][8]:
+                winnerIndices = [i - 1]
 
-tally = sorted(tally, key=lambda a : a[8], reverse=True)
+    # for t in tally:
+    #     print(t)
 
-for i in range(len(tally)):
-    tally[i] = [t[2] if type(t) == list and len(t) == 3 else 'NO DATA' if type(t) == list else t for t in tally[i]]
+    #print(winnerIndices)
+    winnerIndex = decideTie(tally, winnerIndices, getWinnerScore(tournamentHtml))
+    print('\nWINNER: ' + str(tally[winnerIndex]))
 
-header = ['First Name', 'Last Name', 'Golfer 1', 'Earnings 1', 'Golfer 2', 'Earnings 2', 'Golfer 3', 'Earnings 3', 'Total Earnings', 'Tie-Breaker']
+    tally = sorted(tally, key=lambda a : a[8], reverse=True)
 
-df = pd.DataFrame(tally, index=range(1,len(tally)+1), columns=header)
-df.to_csv(tournament + '.csv', index=False, header=True)
-print(df)
+    for i in range(len(tally)):
+        tally[i] = [t[2] if type(t) == list and len(t) == 3 else 'NO DATA' if type(t) == list else t for t in tally[i]]
 
-input("Open <tournament>.csv to see results. Press the ENTER key to exit")
+    header = ['First Name', 'Last Name', 'Golfer 1', 'Earnings 1', 'Golfer 2', 'Earnings 2', 'Golfer 3', 'Earnings 3', 'Total Earnings', 'Tie-Breaker']
+
+    df = pd.DataFrame(tally, index=range(1,len(tally)+1), columns=header)
+    df.to_csv(tournament + '.csv', index=False, header=True)
+    print(df)
+
+
+run()
+action = input("Open <tournament>.csv to see results. Enter the 'q' key to quit")
+while(action != 'q'):
+    if(action != 'q'):
+        print('invalid')
+    action = input()
+quit()
